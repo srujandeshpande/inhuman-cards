5742feb4cd636f42bc62bdbfdc1bff30913341be
Srujan <srujan.deshpande@gmail.com>
2020-07-04 11:23:40 +0530

    Basic React routing

---
+++ src/components/Homepage.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class Homepage extends Component {
+  render() {
+    return (
+      <div>
+          <p>
+            Welcome to Inhuman Cards Homepage
+          </p>
+      </div>
+    );
+  }
+}
+
+export default Homepage;
