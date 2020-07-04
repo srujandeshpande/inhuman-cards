5ba556b4ac233116647edb0a535c497d266aa75e
Srujan <srujan.deshpande@gmail.com>
2020-07-05 00:00:12 +0530

    Homepage with appbar and color

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -1,15 +1,37 @@
-import React, { Component } from 'react';
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
 
-class Homepage extends Component {
-  render() {
-    return (
-      <div>
-          <p>
-            Welcome to Inhuman Cards Homepage
-          </p>
-      </div>
-    );
-  }
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+    textAlign:'center',
+  },
+}));
+
+export default function ButtonAppBar() {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.root}>
+      <AppBar position="static" style={{ background: '#000' }}>
+        <Toolbar>
+          <Typography variant="h6" className={classes.title}>
+            Inhuman Cards.
+          </Typography>
+        </Toolbar>
+      </AppBar>
+    </div>
+  );
 }
-
-export default Homepage;
