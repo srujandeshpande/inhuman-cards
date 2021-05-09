2e979dba60c4d1efc723d130e54c3d11442891a3
Srujan Deshpande <srujan.deshpande@gmail.com>
2021-05-09 12:30:53 +0530

    Fix functional vs class bug

--- src/gamelogic/game.js
+++ src/gamelogic/game.js
@@ -12,43 +12,13 @@ import {
 } from '../redux/gameSlice';
 
 import initializeSockets from './sockets'
-//import store from '../redux/store'
-//import Types from '../redux/actionTypes'
 
-// game class
-
-class Game {
-    constructor(game_id, named) {
-        const dispatch = useDispatch();
-        //var socket = initializeSockets(game_id);
-        //game-variables
-        //this.turn = "x";
-        this.game_id = game_id;
-        this.socket = "Nope";
-        this.socket = initializeSockets(game_id, named);
-        dispatch(joinLobby(this.socket));
-    }
-}
-
-export default Game;
-
-/*
-export default function Game(){
+export default function Game() {
   const dispatch = useDispatch();
   const game_id = useSelector(selectId);
   const named = useSelector(selectName);
-  const soct = useSelector(selectSocket);
-  //var socket = "jo";
-  //console.log("up");
-  if(soct==""){
-    var socket = initializeSockets(game_id);
-    console.log("cow");
-    dispatch(joinLobby(socket));
-  }
-  return(
-    <div>
-    {game_id},{named},{soct}
-    </div>
-  )
+  console.log(game_id, named)
+  var socket = "Nope";
+  var socket = initializeSockets(game_id, named);
+  dispatch(joinLobby(socket));
 }
-*/
